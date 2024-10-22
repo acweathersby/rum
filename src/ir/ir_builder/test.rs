cdd09ccb9440021711ac2397bbc9df5fab4d5f5c
Anthony Weathersby <acweathersby.codes@gmail.com>
2024-10-22 09:08:11 -0600

    Remove unused code

--- src/ir/ir_builder/test.rs
+++
@@ -1,9 +0,0 @@
-use crate::{
-  ir::{
-    ir_build_module::process_module_members,
-    ir_builder::{IRBuilder, SuccessorMode},
-    ir_graph::*,
-  },
-  istring::CachedString,
-  types::*,
-};
--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -1,12 +1,11 @@
 // pub mod ir_block_optimizer;
-pub mod ir_build_module;
-pub mod ir_builder;
+//pub mod ir_build_module;
+//pub mod ir_builder;
 // pub mod ir_optimizer_induction;
 //pub mod ir_block_annotations;
 pub mod ir_block;
 pub mod ir_graph;
 pub mod ir_register_allocator_ssa;
 //pub mod ir_to_register_machine;
-pub mod ir_lowering;
+//pub mod ir_lowering;
 pub mod ir_rvsdg;
-pub mod ir_type_analysis;
